545e6da2d37d71bfa3b589f0fde5f29040434604
Jovon Miller <130570205+millerjovon1@users.noreply.github.com>
2024-03-07 14:21:20 +0900

    last check in

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
+
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext][query]',
